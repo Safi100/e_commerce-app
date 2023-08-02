210aef1e8127c7e5691c416dbddbb5b58e4d63f9
Safi100 <safinafi12@gmail.com>
2023-08-02 15:59:58 +0300

    Create and style responsive nav

---
+++ frontend/src/components/nav/MobileNav.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
+
+const MobileNav = ({open, setOpen}) => {
+    return (
+        <>
+            {open && <div className='backdrop-overlay'></div>}
+            <div className={`mobile-nav ${open && 'active'}`}>
+                <div>
+                    <div className='mobile-nav_header'>
+                        <div className='mobile-nav_header_account'>
+                            <div><a href="/register">Register</a></div>
+                            <div className="line"></div>
+                            <div><a href="/login">Login</a></div>
+                        </div>
+                        <div onClick={()=> setOpen(false)}><CloseOutlinedIcon /></div>    
+                    </div>
+                    <ul className='mobile-nav-ul'>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About us</a></li>
+                        <li><a href="/">How to order</a></li>
+                        <li><a href="/">Deals</a></li>
+                        <li><a href="/">Write a complaint</a></li>
+                        <li><a href="/">My Orders</a></li>
+                        <li className='logout'><button><LogoutOutlinedIcon /> Logout</button></li>
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default MobileNav;
---
+++ frontend/src/components/nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './nav.css'
+const Nav = () => {
+    return (
+        <ul className='nav'>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About us</a></li>
+            <li><a href="/">How to order</a></li>
+            <li><a href="/">Deals</a></li>
+            <li><a href="/">Write a complaint</a></li>
+            <li><a href="/">My Orders</a></li>
+        </ul>
+    );
+}
+
+export default Nav;
