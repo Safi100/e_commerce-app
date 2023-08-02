d94aeaf86cf1bacb99a6f1ac4181d9ca94b02de1
Safi100 <safinafi12@gmail.com>
2023-08-02 16:00:51 +0300

    Create and style search result component

---
+++ frontend/src/components/SearchResult/SearchResult.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './searchResult.css'
+const SearchResult = ({product}) => {
+    return (
+        <a className='searchRes'>
+            <div className='image'><img loading='lazy' src={product.images[0].url} alt={product.title} /></div>
+            <div className='searchRes_info'>
+                <p className='title'>{product.title}</p>
+                {(product.discount > 0 ) ? 
+                <div className='price'>
+                    <span className='PriceToPay'>${product.priceToPay}</span>
+                    <span className='priceBeforeDiscount'>${product.price}</span>
+                    <span className='discount'>{product.discount}%</span>
+                </div> 
+                : 
+                <>
+                <p className='price'>${product.priceToPay}</p>
+                </>}
+                
+            </div>
+        </a>
+    );
+}
+
+export default SearchResult;
