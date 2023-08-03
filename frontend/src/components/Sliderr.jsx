db72b6a18cbf0939afbb264c3ca1235adc5f298b
Safi100 <safinafi12@gmail.com>
2023-08-03 23:22:07 +0300

    Create slider

---
+++ frontend/src/components/Sliderr.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import 'slick-carousel/slick/slick.css'
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick';
+  
+const Sliderr = ({data}) => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    lazyLoad: true,
+    autoplay: true,
+    speed: 700,
+    autoplaySpeed: 5000,
+    cssEase: "linear",
+
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+    return (
+        <div className='wrapper text-center'>
+            <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+          <div>
+            <h3>7</h3>
+          </div>
+          <div>
+            <h3>8</h3>
+          </div>
+        </Slider>   
+        </div>
+    );
+}
+
+export default Sliderr;
