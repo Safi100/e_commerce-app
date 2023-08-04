dbb6d979c7cf65d77e0383db17f247f8d51637ae
Safi100 <safinafi12@gmail.com>
2023-08-04 17:51:50 +0300

    Create getNewArrivals controller

--- backend/controllers/product.js
+++ backend/controllers/product.js
@@ -13,7 +13,23 @@ module.exports.SearchProduct = async (req, res) => {
 module.exports.getFeatures = async (req, res) => {
     try{
         const products = await Product.find({ chose_for_you: true}).limit(6)
+        console.log(products);
         res.status(200).json(products)
+    }catch(e){
+        console.log(e);
+    }
+}
+
+module.exports.getNewArrivals = async (req, res) => {
+    try{
+        var start = new Date();
+        start.setHours(0,0,0,0);
+        
+        var end = new Date();
+        end.setHours(23,59,59,999);
+
+        const product = await Product.find({createdAt: {$gte: start, $lt: end}}).limit(4)
+        res.status(200).json(product)
     }catch(e){
         console.log(e);
     }
