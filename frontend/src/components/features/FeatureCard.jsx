70f084518cc6cd8070d1823a0e4d366295babf51
Safi100 <safinafi12@gmail.com>
2023-08-04 17:57:41 +0300

    modify feature card with deal

--- frontend/src/components/features/FeatureCard.jsx
+++ frontend/src/components/features/FeatureCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './features.css'
+import Deal from '../deal/Deal';
 const FeatureCard = ({feature}) => {
     return (
         <div className='featureCard'>
             <a href={`/product/${feature._id}`} className='featureCard_header'>
+                {feature.discount > 0 && <Deal deal_percent={feature.discount} />}
                 <img className='featureCard_header_image' loading='lazy' src={feature.images[0].url} alt="" />
             </a>
             <div className="featureCard_body mt-3">
