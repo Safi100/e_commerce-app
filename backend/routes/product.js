6cf40329f3de1f5aacf11474d8578c6c9c0030be
Safi100 <safinafi12@gmail.com>
2023-08-04 17:52:04 +0300

    modify product roues

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,13 @@
 const express = require('express')
-const { SearchProduct, getFeatures } = require('../controllers/product')
+const { SearchProduct, getFeatures, getNewArrivals } = require('../controllers/product')
 const router = express.Router({mergeParams: true})
 
 router.route('/search')
 .post(SearchProduct)
 
-router.route('/feature')
+router.route('/cohsen_for_you')
 .get(getFeatures)
 
+router.route('/new_arrival')
+.get(getNewArrivals)
 module.exports = router
