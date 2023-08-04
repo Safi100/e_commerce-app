7b1334c06822c2eb7153792eec17f7cb4d32546b
Safi100 <safinafi12@gmail.com>
2023-08-04 17:51:19 +0300

    fix sllidesToShow issue

--- frontend/src/components/Sliderr.jsx
+++ frontend/src/components/Sliderr.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import 'slick-carousel/slick/slick.css'
 import "slick-carousel/slick/slick-theme.css";
 import Slider from 'react-slick';
+import FeatureCard from './features/FeatureCard';
   
 const Sliderr = ({data}) => {
   var settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 4,
+    // slidesToShow: 4,
     slidesToScroll: 1,
     initialSlide: 0,
     lazyLoad: true,
@@ -20,10 +21,28 @@ const Sliderr = ({data}) => {
 
     responsive: [
       {
+        breakpoint: 3500,
+        settings: {
+          slidesToShow: (data.length < 4) ? data.length : 4,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: (data.length < 3) ? data.length : 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 750,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
           infinite: true,
           dots: true
         }
@@ -31,49 +50,28 @@ const Sliderr = ({data}) => {
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToShow: (data.length < 2) ? data.length : 2,
+          slidesToScroll: 1,
           initialSlide: 2
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: (data.length < 1) ? data.length : 1,
           slidesToScroll: 1
         }
       }
     ]
   };
     return (
-        <div className='wrapper text-center'>
-            <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
-          </div>
+        <Slider {...settings}>
+          {data.length > 0 && data.map(product => (
+            <div className='d-flex justify-content-center' key={product._id}>
+              <FeatureCard feature={product} key={product._id}/>
+            </div>
+          ))}
         </Slider>   
-        </div>
     );
 }
 
