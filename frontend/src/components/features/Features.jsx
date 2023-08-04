7f817bd490547bf13ccadd975e8550994c0b922e
Safi100 <safinafi12@gmail.com>
2023-08-04 17:58:31 +0300

    modify feature section with slider

--- frontend/src/components/features/Features.jsx
+++ frontend/src/components/features/Features.jsx
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios'
-import Carousel from 'react-elastic-carousel';
-import FeatureCard from '../../components/features/FeatureCard'
+import Sliderr from '../Sliderr';
 
-const Features = () => {
+const Features = ({type, heading}) => {
     const [data, setData] = useState([])
 
     useEffect(() => {
         // Function to fetch data using Axios
         const fetchData = async () => {
           try {
-            const response = await Axios.get('http://localhost:8000/product/feature');
-            console.log(response.data);
+            const response = await Axios.get(`http://localhost:8000/product/${type}`);
             setData(response.data)
           } catch (error) {
             console.log(error);
@@ -24,14 +22,12 @@ const Features = () => {
     return (
         <>
         {data.length > 0 && 
-            <div className='feature'>
+            <section className='feature'>
                 <div className="wrapper">
-                    <h2 className='mb-4 text-center'>Chosen for you</h2>
-                        <div className="feature_container">
-                          {data.map(feature => <FeatureCard feature={feature} />)}
-                        </div>
+                  <h2 className='mb-4 text-center'>{heading}</h2>
+                    <Sliderr data={data} />
                 </div>
-            </div>
+            </section>
         }
         </>
     );
