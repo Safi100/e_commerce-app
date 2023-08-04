458ddd498706c2852d9136b08026522a7fbbb856
Safi100 <safinafi12@gmail.com>
2023-08-04 17:58:48 +0300

    Create category component

---
+++ frontend/src/components/categories/CategoryCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CategoryCard = ({category}) => {
+    return (
+        <div className='category_card'>
+            <a href={`/product/${category.CategoryName}`}>
+                <div className='category_card_header'>
+                    <img loading='lazy' className='category_card_header_image' src={category.categoryImage.url} alt={category.CategoryName} />
+                </div>
+                <div className='category_card_body'>
+                    <h4 className='text-center'>{category.CategoryName.toUpperCase()} </h4>
+                </div>
+            </a>
+        </div>
+    );
+}
+
+export default CategoryCard;
