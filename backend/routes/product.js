fadf65fc326ad22bbd6dd8c3ec971f97cef1e8dd
Safi100 <safinafi12@gmail.com>
2023-08-05 22:45:33 +0300

    Create fetch product route

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { SearchProduct, getFeatures, getNewArrivals } = require('../controllers/product')
+const { SearchProduct, getFeatures, getNewArrivals, getProducts } = require('../controllers/product')
 const router = express.Router({mergeParams: true})
 
 router.route('/search')
@@ -10,4 +10,8 @@ router.route('/cohsen_for_you')
 
 router.route('/new_arrival')
 .get(getNewArrivals)
+
+router.route('/:category')
+.get(getProducts)
+
 module.exports = router
