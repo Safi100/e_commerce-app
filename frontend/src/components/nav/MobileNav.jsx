d9563863951f41416eb6879d8c6a1ee54078938d
Safi100 <safinafi12@gmail.com>
2023-08-05 22:45:53 +0300

    Modify links

--- frontend/src/components/nav/MobileNav.jsx
+++ frontend/src/components/nav/MobileNav.jsx
@@ -20,7 +20,7 @@ const MobileNav = ({open, setOpen}) => {
                         <li><a href="/">Home</a></li>
                         <li><a href="/">About us</a></li>
                         <li><a href="/">How to order</a></li>
-                        <li><a href="/">Deals</a></li>
+                        <li><a href="/product/deals">Deals</a></li>
                         <li><a href="/">Write a complaint</a></li>
                         <li><a href="/">My Orders</a></li>
                         <li className='logout'><button><LogoutOutlinedIcon /> Logout</button></li>
--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -6,7 +6,7 @@ const Nav = () => {
             <li><a href="/">Home</a></li>
             <li><a href="/">About us</a></li>
             <li><a href="/">How to order</a></li>
-            <li><a href="/">Deals</a></li>
+            <li><a href="/product/deals">Deals</a></li>
             <li><a href="/">Write a complaint</a></li>
             <li><a href="/">My Orders</a></li>
         </ul>
