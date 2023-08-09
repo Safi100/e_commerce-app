23bd49799c01d67f58487b5452f58f6440870c40
Safi100 <safinafi12@gmail.com>
2023-08-09 20:07:53 +0300

    Setup routes

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import Header from "./components/header/Header";
+import Register from "./pages/Register/Register";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Index from "./pages/index/Index";
+import Products from "./pages/products/Products";
+
+function App() {
+  return (
+    <div className="App">
+      
+      {window.location.pathname !== '/register' && <Header />}
+      <Routes>
+        <Route exact path='/' element={<Index/>} />
+        <Route path='/register' element={<Register/>} />
+        <Route path='/product/:category' element={<Products />} />
+        {/* <Route path='*' element={<NotFound/>} /> */}
+      </Routes>
+      
+    </div>
+  );
+}
+
+export default App;
