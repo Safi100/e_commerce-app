66afae9c90812e7e25cab37be136524fec64f922
safi100 <safinafi12@gmail.com>
2023-08-15 23:50:13 +0300

    Modify products page

--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -3,12 +3,16 @@ import React, { useEffect, useState } from 'react';
 import { useParams } from "react-router-dom";
 import Pagination from '@mui/material/Pagination';
 import Stack from '@mui/material/Stack';
-
+import Product from './Product'
+import './product.css'
+import BrandSideBar from '../../components/brandSideBar/BrandSideBar';
 const Products = () => {
     const {category} = useParams()
     const [page, setPage] = useState(1)
     const [pageCount, setPageCount] = useState(0)
+    const [brands, setBrands] = useState([])
     const [data, setData] = useState([])
+    const [brandSelected, setBrandSelected] = useState([])
 
     const handleChange = (e, value) => {
         setPage(value);
@@ -18,25 +22,35 @@ const Products = () => {
         // Function to fetch data using Axios
         const fetchData = async () => {
         try {
-            const response = await Axios.get(`http://localhost:8000/product/${category}?page=${page}`)
+            const response = await Axios.get(`http://localhost:8000/product/${category}?brand=${brandSelected}& page=${page}`)
             setData(response.data.products)
             setPageCount(response.data.PageCount)
-            console.log(response.data);
+            setBrands(response.data.brands)
         } catch (error) {
             console.log(error);
         }
         };
         // Call the function to fetch data
         fetchData();
-    }, [page])
+    }, [page, brandSelected])
 
     return (
-        <div>
+        <div className='d-flex'>
+        <div className='product wrapper' >
+            <div className='product_container'>
+                {
+                    data.map(product => (
+                        <Product product={product} key={product._id} />
+                        )
+                        )}
+            </div>
+            <div className='d-flex justify-content-center mt-5'>
             <Stack spacing={2}>
-                <a>Page: {page}</a>
-                {pageCount > 0 && <Pagination count={pageCount} page={page} onChange={handleChange} color='secondary' />}
-                
+                {pageCount > 0 && <Pagination count={pageCount} page={page} onChange={handleChange} color='primary' />}
             </Stack>
+            </div>
+        </div>
+        <BrandSideBar products={data} brands={brands} brandSelected={brandSelected} setBrandSelected={setBrandSelected}/>
         </div>
     );
 }
