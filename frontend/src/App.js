21b9d93a6588d83fa5177c249e68e819368db99c
safi100 <safinafi12@gmail.com>
2023-08-19 05:08:44 +0300

    Create not found page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Register from "./pages/Register/Register";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Index from "./pages/index/Index";
 import Products from "./pages/products/Products";
+import NotFound from "./pages/notFound/NotFound";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
         <Route exact path='/' element={<Index/>} />
         <Route path='/register' element={<Register/>} />
         <Route path='/product/:category' element={<Products />} />
-        {/* <Route path='*' element={<NotFound/>} /> */}
+        <Route path='*' element={<NotFound/>} />
       </Routes>
       
     </div>
