238a299a6aec7bc7d574edc872c8cb4e95525b30
safi100 <safinafi12@gmail.com>
2023-08-28 23:37:26 +0300

    Check path for header & footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,28 @@
 import Header from "./components/header/Header";
 import Register from "./pages/Register/Register";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, useLocation} from 'react-router-dom'
 import Index from "./pages/index/Index";
 import Products from "./pages/products/Products";
 import NotFound from "./pages/notFound/NotFound";
 import ProductProfile from "./pages/productProfile/ProductProfile";
+import Footer from "./components/footer/Footer";
+import Login from "./pages/login/Login";
 
 function App() {
+  const path = useLocation()
+  const isLoginPageOrRegisterPage = path.pathname === '/register' || path.pathname === '/login';
   return (
     <div className="App">
-      
-      {window.location.pathname !== '/register' && <Header />}
+      {!isLoginPageOrRegisterPage && <Header />}
       <Routes>
         <Route exact path='/' element={<Index/>} />
+        <Route path='/login' element={<Login/>} />
         <Route path='/register' element={<Register/>} />
         <Route path='/product-category/:category' element={<Products />} />
         <Route path='/product-profile/:id' element={<ProductProfile />} />
         <Route path='*' element={<NotFound/>} />
       </Routes>
+      {!isLoginPageOrRegisterPage && <Footer />}
       
     </div>
   );
