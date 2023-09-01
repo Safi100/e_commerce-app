6f47cade9d99db1a06dbc14dbb1834802d128d07
safi100 <safinafi12@gmail.com>
2023-09-01 23:21:59 +0300

    Create auth add_review component

---
+++ frontend/src/components/reviews/Reviews.jsx
@@ -0,0 +1,115 @@
+import React, { useEffect, useState } from 'react';
+import './reviews.css'
+import Review from './Review';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Axios from 'axios'
+
+const Reviews = ({user, reviews, productID, setRefetch, reFetch}) => {
+    const [rating, setRating] = useState(0)
+    const [selectedImage, setSelectedImage] = useState('');
+    const [selectedImageDisplay, setSelectedImageDisplay] = useState('');
+    const [error, setError] = useState('')
+    const [success, setSuccess] = useState('')
+    const [formData, setFormData] = useState({
+        title: '',
+        body: '',
+    })
+    
+    useEffect(()=> setRefetch(false))
+
+    const handleImageChange = (e) => {
+        setSelectedImage(e.target.files[0])
+        if(e.target.files.length !== 0){
+            setSelectedImageDisplay(URL.createObjectURL(e.target.files[0]));
+        }
+    };
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        const text = value.trimStart();
+        setFormData((prevData) => ({
+          ...prevData,
+          [name]: text
+        }))
+    }
+    const AddReview = (e) => {
+        e.preventDefault()
+        setError('')
+        setSuccess('')
+        if(rating === 0) {
+            return setError("Please rate before submit")
+        } 
+        let Data = new FormData()
+        Data.append('title', formData.title)
+        Data.append('body', formData.body)
+        Data.append('review_image', selectedImage)
+        Data.append('rating', rating)
+        Data.append('productID', productID)
+
+        Axios.post('http://localhost:8000/review/addReview', Data, {
+            headers: {authorization: "Bearer " + user.token}
+        })
+        .then(res => {
+            if(res.status === 200){
+                e.target.reset()
+                setFormData({
+                    title: '',
+                    body: '',
+                })
+                setRating(0)
+                setSelectedImage('')
+                setSelectedImageDisplay('')
+                setError('')
+                setSuccess('Review added successfully')
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+            setError(err.response.data.error)
+        })
+    }
+    return (
+        <div className='review_dev mt-5'>
+            {console.log(reviews)}
+            {user &&  <form className='review-form-container__parent' onSubmit={AddReview}>
+                <div className="review-form-container">
+                    <h2 className="review-header">Write a Review</h2>
+                    <div className="rating-stars">
+                    <Box sx={{ '& > legend': { mt: 2 },}} >
+                        <Rating value={rating} name='rating' onChange={(e, value)=> setRating(value)} required/>
+                    </Box>
+                    </div>
+                    <input type="text" onChange={handleInputChange}  className='review-text' value={formData.title} name='title' placeholder='Write your review heading...' required/>
+                    <textarea value={formData.body} onChange={handleInputChange} name='body' className="review-textarea" placeholder="Write your review here..."  required/>
+                    <div className="image-upload mb-4">
+                        <div className='control_image mb-2'>
+                            <p className='mb-0'>*You can only upload one image</p>
+                            {selectedImage && <button onClick={() => {setSelectedImage(''); setSelectedImageDisplay('') }} 
+                            className='btn btn-outline-danger' type='button'>delete Image</button>}
+                        </div>
+                        <input
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                        />
+                        {selectedImage && <img className='review_SelectedImage' src={selectedImageDisplay} alt={selectedImage.name}/>}
+                    </div>
+                    {error && <p className='text-danger'>{error}</p>}
+                    {success && <p className='text-success'>{success}</p>}
+                    <button className="submit-button">Submit Review</button>
+                </div>
+            </form>}
+            {reviews?.length > 0 && <div className='reviews'>
+                 <h3 className='fs-4 mb-3'>All Reviews ({reviews.length})</h3>
+                {reviews?.map(review => (
+                    <Review review={review} user={user} key={review._id} 
+                    reFetch={reFetch} setRefetch={setRefetch}
+                    />
+                ))}
+            </div>
+            }
+        </div>
+    );
+}
+
+export default Reviews;
