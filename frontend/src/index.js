9223bfae0bd2e329715d0eff747966ca6fbc73cd
safi100 <safinafi12@gmail.com>
2023-09-09 22:14:13 +0300

    Add cartContextProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,14 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
-
+import { CartContextProvider } from './context/Cart';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <AuthContextProvider>
+      <CartContextProvider>
       <Router>
         <App />
       </Router>
+      </CartContextProvider>
     </AuthContextProvider>
 );
