39cbaf02637d4e10d6a4a2dfa22fb49a5210c750
safi100 <safinafi12@gmail.com>
2023-09-16 23:24:26 +0300

    render CustomerContextProvider

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,17 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
-import { CartContextProvider } from './context/Cart';
+import { CartContextProvider } from './context/CartContext';
+import { CustomerContextProvider } from './context/CustomerContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <AuthContextProvider>
       <CartContextProvider>
-      <Router>
-        <App />
-      </Router>
+        <CustomerContextProvider>
+          <Router>
+            <App />
+          </Router>
+        </CustomerContextProvider>
       </CartContextProvider>
     </AuthContextProvider>
 );
