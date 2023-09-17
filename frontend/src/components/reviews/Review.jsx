138b45fbc4db2cb79ed26b8e7cf6a1eeaffb50d9
safi100 <safinafi12@gmail.com>
2023-09-17 20:34:54 +0300

    Modify product review component

--- frontend/src/components/reviews/Review.jsx
+++ frontend/src/components/reviews/Review.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './reviews.css'
 import Rating from '@mui/material/Rating';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import Axios from 'axios'
 
-
-const Review = ({review, user, refetch, setRefetch}) => {
-
+const Review = ({review, user}) => {
+    const [open, setOpen] = useState(false)
+    const handleClick = (open) => {
+        setOpen(!open)
+    }
     const deleteReview = (e) => {
         Axios.delete(`http://localhost:8000/review/deleteReview/${review._id}`, {
             headers: {authorization: "Bearer " + user.token}
         })
         .then(res => {
-            setRefetch(!refetch)
             if(res.status === 200){
-                setRefetch(true)
+                
             }
         })
     }
@@ -24,9 +28,18 @@ const Review = ({review, user, refetch, setRefetch}) => {
                 <div className="review-rating"><Rating name="read-only" value={review.rating} readOnly /></div>
             </div>
             <p className="review-author">By {`${review?.author.first_name} ${review?.author.last_name}`}</p>
-            <p className="review-body">{review.body}</p>
             <p className="review-date">Posted on {new Date(review.createdAt).toLocaleString('en-US', { year: 'numeric', month: 'long', day: 'numeric'})}</p>
-            {review.image.url && <img className="review-image" src={review.image.url} />}
+            <div className="review_body">
+                <div className={((review.body.length < 200) && !review.image.url) ? "" : (!open && "cut")} >
+                    <p>{review.body}</p>
+                    {review.image.url && <img className='review_image' src={review.image.url} alt="" /> }
+                </div>
+            </div>
+            <div className="review_footer">
+                {((review.body.length < 200) && !review.image.url)  ? "" : <p onClick={()=> {handleClick(open)}} 
+                className='read_more_btn'>{(open === false) ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon /> } 
+                <span className='read_more'>Read {(open === false) ? "more" : "less" }</span></p>}
+            </div>
             {user.id === review.author._id && <button className='btn btn-danger' onClick={deleteReview}>delete</button>}
       </div>
     );
