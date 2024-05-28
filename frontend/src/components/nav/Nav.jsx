75d9c47b77b647f82c117857829882a3312d6f7f
safi100 <safinafi12@gmail.com>
2024-05-28 04:01:02 +0300

    change var name

--- frontend/src/components/nav/Nav.jsx
+++ frontend/src/components/nav/Nav.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './nav.css'
-const Nav = ({user}) => {
+const Nav = ({currentUser}) => {
     return (
         <ul className='nav'>
             <li><a href="/">Home</a></li>
             <li><a href="/">About us</a></li>
             <li><a href="/">How to order</a></li>
             <li><a href="/product-category/deals">Deals</a></li>
-            {user && <li><a href="/my-account">My Account</a></li>}
+            {currentUser && <li><a href="/my-account">My Account</a></li>}
         </ul>
     );
 }
