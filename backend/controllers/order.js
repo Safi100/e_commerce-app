b4ac1d56e110f8e238f62b25974272688a23b029
safi100 <safinafi12@gmail.com>
2024-05-30 04:48:11 +0300

    Create checkout session function and route

---
+++ backend/controllers/order.js
@@ -0,0 +1,46 @@
+const Stripe = require('stripe');
+const Customer = require('../models/customer');
+const { model } = require('mongoose');
+
+module.exports.create_checkout_session = async (req, res, next) => {
+    try {
+        const customer = await Customer.findById(req.user.id).populate('cart.items.product').exec();
+        
+        if (customer.cart.items.length < 1) {
+            throw new Error('Cart is empty');
+        }
+        // delivery cost is free for orders greater than or equal $300.00, otherwise $20.00.
+        let DeliveryCost = 0;
+        if(customer.cart.total < 300) {
+            DeliveryCost = 20;
+        } 
+        const totalAmount = (customer.cart.total + DeliveryCost);
+
+        const stripe = new Stripe(process.env.STRIPE_KEY);
+
+        const line_items = customer.cart.items.map(item => ({
+            price_data: {
+                currency: "usd",
+                product_data: {
+                    name: item.product.title,
+                    images: [item.product.images[0].url],
+                },
+                unit_amount: Math.round(item.product.price * (1 - item.product.discount / 100) * 100),
+            },
+            quantity: item.quantity,
+        }));
+
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ['card'],
+            line_items: line_items,
+            mode: 'payment',
+            success_url: `${process.env.BASE_URL}/success-payment`,
+            cancel_url: `${process.env.BASE_URL}/failed-payment`,
+        });
+
+        res.status(200).json({id: session.id});
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+        console.log(e);
+    }
+};
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { create_payment_intent } = require('../controllers/order')
+const { create_checkout_session } = require('../controllers/order')
 const router = express.Router({mergeParams: true})
 const { authorization } = require('../middleware')
 
-router.route('/create-payment-intent/')
-.post(authorization, create_payment_intent)
+router.route('/create-checkout-session')
+.post(authorization, create_checkout_session)
 
 module.exports = router
